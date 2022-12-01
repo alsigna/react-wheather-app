4a2f2c774493a35a615ce4e285bdc2d24e4020ba
alsigna <ignatov.alx@gmail.com>
2022-12-01 20:58:02 +0500

    tmp files

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "94eb8c69504cc008642b268104f2f42a";
+export const WEATHER_API_KEY = "";
   
   
